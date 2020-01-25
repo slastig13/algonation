7a29fdefe9844d7d57a56172f16f6de61fd1f292
Samantha Lastig <slastig13@gmail.com>
2020-01-25 15:10:56 -0500

    WIP find closest value BST

---
+++ findClosestValBST.js
@@ -0,0 +1,25 @@
+// Prompt: You are given a BST consisting of BST nodes. Each BST node has an integer value stored in a property called "value" and two children nodes stored in properties called "left" and "right". You are also given a target integer value. Write a function that finds the closest value to that target value contained in the BST. Assume that there will only be one closest value.
+
+function findClosestValueInBst(tree, target) {
+  let currNode = tree
+  let bestCase = {diff: Infinity, value: null}
+
+  while (currNode) {
+    let currDiff = Math.abs(target - currNode.value)
+    if (currDiff === 0) return currNode.value
+    if (target >= currNode.value && currNode.right) {
+      if (currDiff < bestCase.diff) {
+        bestCase.diff = currDiff
+        bestCase.value = currNode.value
+      }
+      currNode = currNode.right
+    } else if (target < currNode.value && currNode.left) {
+      if (currDiff < bestCase.diff) {
+        bestCase.diff = currDiff
+        bestCase.value = currNode.value
+      }
+      currNode = currNode.left
+    }
+  }
+  return bestCase.value
+}
